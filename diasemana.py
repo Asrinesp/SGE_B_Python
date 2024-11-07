6b8d8b49a341708068cd3674d8a5d6b614f377e4
Carlos Pérez <2024_carlos.perez@iticbcn.cat>
2024-11-07 11:39:33 +0100

    ejercicio 2 terminado

---
+++ diasemana.py
@@ -0,0 +1,17 @@
+dia_semana = input("Introduce un dia de la semana: ")
+if dia_semana == "lunes":
+    print("Hoy es lunes")
+elif dia_semana == "martes":
+    print("Hoy es martes")
+elif dia_semana == "miercoles":
+    print("Hoy es miercoles")
+elif dia_semana == "jueves":
+    print("Hoy es jueves")
+elif dia_semana == "viernes":
+    print("Hoy es viernes")
+elif dia_semana == "sabado":
+    print("Hoy es sabado")
+elif dia_semana == "domingo":
+    print("Hoy es domingo")
+else:
+    print("Introduce un dia de la semana correcto en minusculas")
