b72a0f4eac8f59e3ef0c406e424c718621af7b64
Carlos Pérez <2024_carlos.perez@iticbcn.cat>
2024-11-21 12:03:42 +0100

    Terminado

---
+++ numeros1-10.py
@@ -0,0 +1,13 @@
+num = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+numWhile = 1
+
+print("For:")
+for i in num:
+    print(i)
+    i += 1
+
+print()
+print("While:")
+while numWhile <= 10:
+    print(numWhile)
+    numWhile += 1
